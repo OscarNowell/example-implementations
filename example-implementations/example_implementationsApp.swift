8b4d78299178ec9f23af2a1c65c0d7f3a9a8b705
oscar.nowell <oscar.nowell@gmail.com>
2025-10-16 14:04:27 +0100

    Initial Commit

---
+++ example-implementations/example_implementationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  example_implementationsApp.swift
+//  example-implementations
+//
+//  Created by Oscar Nowell on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct example_implementationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
