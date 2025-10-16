11a20c94fb61233b57e3cb03cbb93a6e86a357f8
oscar.nowell <oscar.nowell@gmail.com>
2025-10-16 14:15:33 +0100

    test: Verify ApiClient has baseUrl String

---
+++ example-implementations/BasicNetworkAPIClient/ApiClient.swift
@@ -0,0 +1,13 @@
+//
+//  ApiClient.swift
+//  example-implementations
+//
+//  Created by Oscar Nowell on 16/10/2025.
+//
+
+import Foundation
+
+class ApiClient {
+    
+    let baseUrl: String = ""
+}
---
+++ example-implementationsTests/BasicNetworkAPIClientTests/ApiClientTests.swift
@@ -0,0 +1,29 @@
+//
+//  ApiClientTests.swift
+//  example-implementationsTests
+//
+//  Created by Oscar Nowell on 16/10/2025.
+//
+
+import XCTest
+
+@testable import example_implementations
+
+final class ApiClientTests: XCTestCase {
+    
+    private var apiClient: ApiClient!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        apiClient = ApiClient()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    // test base URL exists
+    func test_apiClient_hasBaseUrl() throws {
+        XCTAssertNotNil(apiClient.baseUrl)
+    }
+}
