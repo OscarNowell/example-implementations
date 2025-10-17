627b23c6f87fc60989d3d7d480f2c1c7816239e8
oscar.nowell <oscar.nowell@gmail.com>
2025-10-17 15:14:06 +0100

    test, refactor: Debouncer uses Task for delay. Tests use delay

--- example-implementations/Debouncer/Debouncer.swift
+++ example-implementations/Debouncer/Debouncer.swift
@@ -7,10 +7,30 @@
 
 import Foundation
 
-class Debouncer {
+class Debouncer{
+    
+    private let delay: Duration
+    private var currentTask: Task<Void, Never>?
+    
+    init(delay: Duration) {
+        self.delay = delay
+    }
     
     // function for checking if the last key stroke was entered OVER 300ms ago
-    func canMakeNetworkCall() -> Bool {
-        return true
+    // IF 300ms has passed, should execute API call with search query
+    func debounce(action: @escaping () async -> Void) {
+        currentTask?.cancel()
+        
+        currentTask = Task {
+            do {
+                
+                try await Task.sleep(for: delay)
+                
+                await action()
+                
+            } catch {
+                // task is cancelled
+            }
+        }
     }
 }
